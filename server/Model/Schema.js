6d571ccef39e961d8c1e6d4aab8ea8c680531b34
sakshikhalode22 <55319326+sakshikhalode22@users.noreply.github.com>
2024-06-10 11:09:12 +0530

    Revert "Create User API #2"

--- server/Model/Schema.js
+++ server/Model/Schema.js
@@ -9,119 +9,39 @@ mongoose
 
 
 const UserSchema = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      unique: true,
+    {
+        id: {
+        type: Number,
+        unique: true,
+        },
+        name: {
+        type: String,
+        },
+        address: {
+        type: String,
+        },
+        email: {
+        type: String,
+        unique:true,
+        required: true,
+        },
+        phoneNo: {
+        type: Number,
+        },
+        grade:{
+        type: Number,
+        },
+        password: {
+        type: String,
+        },
     },
-    name: {
-      type: String,
-      required: true,
-    },
-    address: {
-      type: String,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-    },
-    phoneNo: {
-      type: Number,
-    },
-    grade: {
-      type: String,
-    },
-    school: {
-      type: String,
-    },
-    password: {
-      type: String,
-    },
-  },
-  {
-    timestamps: {
-      createdAt: true,
-      updatedAt: true,
-    },
-  }
-);
-
-const QuestionSchema = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      unique: true,
-    },
-    question: {
-      type: String,
-    },
-    options: {
-      type: Array,
-    },
-    answer: {
-      type: String,
-    },
-    level: {
-      type: String,
-    },
-    tag:{
-      type: String,
-    },
-    grade:{
-      type:String,
+    {
+        timestamps: {
+        createdAt: true,
+        updatedAt: true,
+        },
     }
-  },
-  {
-    timestamps: {
-      createdAt: true,
-      updatedAt: true,
-    },
-  }
-);
+    );
+const Users = mongoose.model('Users', UserSchema);
 
-const ExamsSchema = new mongoose.Schema(
-  {
-    id: {
-      type: Number,
-      unique: true,
-    },
-    userid:{
-      type: Number,
-    },
-    examname: {
-      type: String,
-    },
-    date: {
-      type: Date,
-    },
-    tag: {
-      type: String,
-    },
-    score: {
-      type: Number,
-    },
-    result:{
-      type: String,
-    }
-  },
-  {
-    timestamps: {
-      createdAt: true,
-      updatedAt: true,
-    },
-  }
-);
-
-
-const Users = mongoose.model("Users", UserSchema);
-
-module.exports = Users;
-
-const Questions = mongoose.model("Questions", QuestionSchema);
-
-module.exports = Questions;
-
-const Exams = mongoose.model("Exams", ExamsSchema);
-
-module.exports = Exams;
+module.exports =Users
--- server/Utilities/helpers.js
+++ server/Utilities/helpers.js
@@ -1,20 +1,7 @@
 const Users = require("../Model/Schema");
-const Exams = require("../Model/Schema");
 
 exports.generateUserId = async () => {
   const user = await Users.find({});
   const Id = 1 + user.length;
   return Id;
 };
-
-exports.generateExamId = async () =>{
-  const exams = await Exams.find({});
-  const Id = 1 + exams.length;
-  return Id;
-}
-
-exports.generateExamName = async (date) =>{
-  const dateArray = date.split(" ");
-  const examName = dateArray[0] + dateArray[1] + dateArray[2];
-  return examName;
-}
